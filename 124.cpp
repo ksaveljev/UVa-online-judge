0e283cb6d7d8ee095ca688fc7ddf39b1234005c1
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2011-05-23 11:50:05 +0300

    124 solved

--- 124.cpp
+++ 124.cpp
@@ -1,77 +1,83 @@
 #include <iostream>
-#include <vector>
 #include <map>
-#include <sstream>
-#include <string>
+#include <algorithm>
 using namespace std;
 
-vector<char> c;
-multimap<char,char> dep;
-vector<char> cur;
-bool *taken;
-pair<multimap<char,char>::iterator,multimap<char,char>::iterator> it;
-vector<char>::iterator vit;
+int n;
+char var[20];
+multimap<char,char> m;
+char current[20];
+bool taken[20];
+bool found;
 
-bool notBad (char a) {
-	it = dep.equal_range(a);
-	for (multimap<char,char>::iterator i = it.first; i != it.second; i++) {
-		vit = find (cur.begin(), cur.end(), (*i).second);
-		if (vit != cur.end())
-			return false;
-	}
-	
-	return true;
+bool fit(char c, int depth) {
+  multimap<char,char>::iterator it;
+  pair<multimap<char,char>::iterator,multimap<char,char>::iterator> ret;
+
+  ret = m.equal_range(c);
+  for (it = ret.first; it != ret.second; it++) {
+    for (int i = 0; i < depth; i++) {
+      if (current[i] == it->second)
+        return false;
+    }
+  }
+
+  return true;
 }
 
-void doStuff(int n) {
-	if (n < c.size()) {
-		for (int i = 0; i < c.size(); i++) {
-			if (taken[i] == false && notBad(c[i])) {
-				cur.push_back(c[i]);
-				taken[i] = true;
-				doStuff(n+1);
-				cur.pop_back();
-				taken[i] = false;
-			}
-		}
-	} else {
-		for (int i = 0; i < cur.size(); i++)
-			cout << cur[i];
-		cout << endl;
-	}
+void solve(int depth) {
+  if (depth == n) {
+    for (int i = 0; i < n; i++) {
+      cout << current[i];
+    }
+    cout << endl;
+    found = true;
+    return;
+  }
+
+  for (int i = 0; i < n; i++) {
+    if (!fit(var[i], depth) || taken[i])
+      continue;
+    taken[i] = true;
+    current[depth] = var[i];
+    solve(depth + 1);
+    taken[i] = false;
+  }
 }
 
-int main (void) {
-	string input;
-	int count = 0;
-	
-	while (getline(cin, input)) {
-		count++;
-		c.clear();
-		dep.clear();
-		
-		stringstream ss, sss;
-		ss << input;
-		char temp, a, b;
-		
-		while (ss >> temp) c.push_back(temp);
-		sort (c.begin(), c.end());
-		
-		taken = new bool[c.size()];
-		for (int i = 0; i < c.size(); i++) taken[i] = false;
-		
-		getline(cin, input);
-		
-		
-		sss.str("");
-		sss << input;
-		while (sss >> a >> b) dep.insert(pair<char,char>(a,b));
-		
-		if (count > 1) cout << endl;
-		doStuff(0);
-		
-		delete taken;
-	}
-	
-	return 0;
+int main(void) {
+  char a, b;
+  bool first = true;
+
+  while (cin.peek() != -1) {
+    n = 0;
+    fill (taken, taken + 20, false);
+    m.clear();
+
+    if (!first)
+      cout << endl;
+
+    while (cin.peek() != '\n') {
+      cin >> a;
+      var[n] = a;
+      n++;
+    }
+
+    sort(var, var + n);
+
+    cin.ignore(100, '\n');
+
+    while (cin.peek() != '\n' && cin.peek() != -1) {
+      cin >> a >> b;
+      m.insert(make_pair(a, b));
+    }
+
+    cin.ignore(100, '\n');
+
+    solve(0);
+
+    first = false;
+  }
+
+  return 0;
 }
