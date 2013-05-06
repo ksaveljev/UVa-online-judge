6e2b8f2d9eeb3067826f030be2dc673316bd5d0d
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2013-05-06 08:19:35 +0300

    a few more macros added to skeleton

--- skeleton.cpp
+++ skeleton.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 
+#define pow2(i) (1<<i)
+#define bit(i) (1<<i)
+#define isOdd(i) (i&1)
+#define isEven(i) (!(i&1))
+#define sz(i) i.size()
 #define REP(i, b, n) for (int i = b; i < n; i++)
 #define REPI(i, b, n) for (int i = b; i <= n; i++)
 #define rep(i, n) REP(i, 0, n)
