80260aad243d5b18da991b0401a12c43f3c9ff11
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2014-02-14 13:38:20 +0200

    12708.cpp

---
+++ 12708.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+#define pow2(i) (1<<i)
+#define bit(i) (1<<i)
+#define isOdd(i) (i&1)
+#define isEven(i) (!(i&1))
+#define sz(i) i.size()
+#define REP(i, b, n) for (int i = b; i < n; i++)
+#define REPI(i, b, n) for (int i = b; i <= n; i++)
+#define rep(i, n) REP(i, 0, n)
+#define repi(i, n) REPI(i, 0, n)
+
+int main(void) {
+    int t;
+    unsigned long long n;
+
+    cin >> t;
+
+    while (t--) {
+        cin >> n;
+
+        if (n % 2 == 1) n--;
+
+        cout << n / 2 << endl;
+    }
+
+    return 0;
+}
