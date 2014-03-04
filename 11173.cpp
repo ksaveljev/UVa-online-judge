3e1223b8e806d93cd76c6832c488147d6ffb0ea2
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2014-03-04 13:10:34 +0200

    11173 solved

---
+++ 11173.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+#define pow2(i) (1<<i)
+#define bit(i) (1<<i)
+#define isOdd(i) (i&1)
+#define isEven(i) (!(i&1))
+#define sz(i) i.size()
+#define REP(i, b, n) for (int i = b; i < n; i++)
+#define REPI(i, b, n) for (int i = b; i <= n; i++)
+#define rep(i, n) REP(i, 0, n)
+#define repi(i, n) REPI(i, 0, n)
+
+int main(void) {
+    int N, n, k;
+
+    cin >> N;
+
+    while (N--) {
+        cin >> n >> k;
+        cout << (k ^ (k >> 1)) << endl;
+    }
+
+    return 0;
+}
