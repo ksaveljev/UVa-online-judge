a226f5b31e485c1adba7b8a50d0f1015fada4f90
Konstantin Saveljev <konstantin.saveljev@bolt.eu>
2021-02-04 22:18:14 +0200

    11388 solved

---
+++ 11388.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <stack>
+#include <iomanip>
+#include <cmath>
+#include <cstring>
+#include <numeric>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define loop(a) for(int i = 0; i < a; i++)
+#define loopv(i,a) for (int i = 0; i < a; i++)
+#define rep(i,a,b) for (int i = a; i < b; i++)
+#define all(x) (x).begin(), (x).end()
+#define prDouble(x) cout << fixed << setprecision(10) << x
+#define goog(tno) cout << "Case #" << tno << ": "
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+int main(void) {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int G, L;
+        cin >> G >> L;
+
+        if (L % G == 0) {
+            cout << G << " " << L << endl;
+        } else {
+            cout << -1 << endl;
+        }
+    }
+
+    return 0;
+}
